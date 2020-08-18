cb505311abfce9d1aa39a38b8e8f1d7b601a9f7f
operah24 <66219854+operah24@users.noreply.github.com>
2020-08-17 20:30:35 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,52 @@
+// 1.Select the section with an id of container without using querySelector.
+document.getElementById("container");
+
+// 2. Select the section with an id of container using querySelector.
+document.querySelector("#container");
+
+// 3. Select all of the list items with a class of "second".
+document.getElementsByClassName("second");
+
+// 4. Select a list item with a class of third, but only the list item inside of the ol tag.
+document.querySelector("ol .third");
+
+
+// 5. Give the section with an id of container the text "Hello!".
+var firstSection = document.getElementById("container")
+firstSection.innerText = "Hello!";
+
+
+// 6. Add the class main to the div with a class of footer.
+var footerDiv = document.querySelector(".footer")
+footerDiv.classList.add("main");
+
+// 7. Remove the class main on the div with a class of footer.
+footerDiv.classList.remove("main")
+
+// 8.Create a new li element.
+var newLi = document.createElement("li")
+
+
+//   9. Give the li the text "four".
+newLi.innerText = "four";
+
+// 10. Append the li to the ul element.
+var list = document.querySelector("ul")
+list.appendChild(newLi);
+
+// 11. Loop over all of the lis inside the ol tag and give them a background color of "green".
+var ols = document.querySelectorAll("ol");
+for(var i = 0; i<ols.length; i++){
+    ols[i].style.backgroundColor = "green";
+}
+
+//  12. Remove the div with a class of footer.
+    var footer = document.getElementsByClassName("footer");
+    footer.removeChild();
+
+
+
+
+
+
+
